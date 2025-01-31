ce025cfae6c7d2fad17fff331425261cad5a37e3
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-31 01:14:01 -0500

    Code clean up

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,29 +8,48 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
 
-    [SerializeField] private bool isMoving; 
     private float xInput; 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
+    
     void Update()
+    {
+        Movement();
+
+        CheckInput();
+
+        AnimatorControllers();
+    }
+
+    private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            Jump();
         }
+    }
 
-        isMoving = rb.linearVelocity.x != 0; 
+    private void Movement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.linearVelocity.x != 0; 
 
         anim.SetBool("isMoving", isMoving);
     }
